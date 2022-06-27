2693bc41753e764308bc566c9c8b10f6f170099c
Isala Piyarisi <code@isala.me>
2022-06-27 10:35:27 +0530

    Tested for Large payload

--- postman-echo-servers/go/service.go
+++ postman-echo-servers/go/service.go
@@ -6,12 +6,11 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"time"
 )
 
 func main() {
-	os.Setenv("GODEBUG", "http2client=0")
-	fmt.Printf("Starting Postman Echo with HTTP/1\n")
+	// os.Setenv("GODEBUG", "http2client=0")
+	fmt.Printf("Starting Postman Echo with HTTP/2\n")
 	fmt.Printf("pid: %d\n", os.Getpid())
 
 	http.HandleFunc("/https", func(w http.ResponseWriter, r *http.Request) {
@@ -48,8 +47,34 @@ func main() {
 		fmt.Printf("Body : %s\n", body)
 	})
 
+	http.HandleFunc("/https/large", func(w http.ResponseWriter, r *http.Request) {
+		data, err := os.Open("../data.blob")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer data.Close()
+
+		req, err := http.NewRequest("PUT", "https://postman-echo.com/put/data.blob", data)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Printf("Error %s", err)
+			return
+		}
+		fmt.Printf("Body : %s\n", body)
+	})
+
 	http.HandleFunc("/empty", func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(2 * time.Second)
+		return
 	})
 
 	log.Fatal(http.ListenAndServe(":9092", nil))
